cab632f98ef4be515884ee9d47e5dbcb71c421a8
metaldrummer610@gmail.com <metaldrummer610@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-02-19 19:10:16 +0000

    Initial commit of the project files. I believe that this all that we need at the moment to get started.

---
+++ src/edu/spsu/cs3243/Loader.java
@@ -0,0 +1,10 @@
+package edu.spsu.cs3243;
+
+public class Loader {
+
+	public static void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/edu/spsu/cs3243/RAM.java
@@ -0,0 +1,10 @@
+package edu.spsu.cs3243;
+
+public class RAM {
+
+	public static void initalize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
