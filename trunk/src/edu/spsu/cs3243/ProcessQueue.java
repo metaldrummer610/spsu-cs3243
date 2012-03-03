d034e18e672279cdc1022ef6c5810e55e158e16b
metaldrummer610@gmail.com <metaldrummer610@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-03-03 21:15:13 +0000

    Process Queue

--- trunk/src/edu/spsu/cs3243/ProcessQueue.java
+++ trunk/src/edu/spsu/cs3243/ProcessQueue.java
@@ -1,5 +1,20 @@
 package edu.spsu.cs3243;
 
+import java.util.ArrayList;
+
 public class ProcessQueue {
 
+	private ArrayList<PCB> processes;
+
+	public ProcessQueue() {
+		processes = new ArrayList<PCB>();
+	}
+
+	public void add(PCB pcb) {
+		processes.add(pcb);
+	}
+
+	public PCB get(int index) {
+		return processes.get(index);
+	}
 }
