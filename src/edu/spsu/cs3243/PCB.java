3e32f3e4ea523f0364d002760b41fc48cc689b5d
dbbutcher1@gmail.com <dbbutcher1@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-03-03 22:02:53 +0000

    checking in

--- src/edu/spsu/cs3243/PCB.java
+++ src/edu/spsu/cs3243/PCB.java
@@ -1,17 +1,41 @@
 package edu.spsu.cs3243;
 
+import java.util.ArrayList;
+
 public class PCB 
 {
-	int pid, size, priority, inputBuffer, outputBuffer, tempBuffer;
+	int pid, processSize, priority, inputBufferSize, outputBufferSize, tempBufferSize, pc, pageFaults, dataSize, dataMemLoc,
+	dataDiskLoc, instMemLoc, instDiskLoc, cyclesRan, cyclesWaited, IOCount;
+	long lastStateSwitch, realWaitTime, realRunTime, faultTime;
+	String[] inputBuffer, outputBuffer, tempBuffer;
+	int[] registers;
+	ArrayList<Integer> pageTable;
+	ArrayList<Boolean> pageValidity;
 	
 	public PCB()
 	{
 		pid = -1;
-		size = -1;
+		processSize = -1;
 		priority = -1;
-		inputBuffer = -1;
-		outputBuffer = -1;
-		tempBuffer = -1;
+		inputBufferSize = -1;
+		outputBufferSize = -1;
+		tempBufferSize = -1;
+		pc = -1;
+		dataDiskLoc=-1;
+		instDiskLoc=-1;
+		dataSize=-1;
+		dataMemLoc=-1;
+		instMemLoc=-1;	
+		cyclesRan=0;
+		cyclesWaited=0;
+		realRunTime=0;
+		realWaitTime=0;
+		IOCount=0;
+		pageTable=new ArrayList<Integer>();
+		pageValidity=new ArrayList<Boolean>();
+		pageFaults=0;
+		faultTime=0;
+		lastStateSwitch=0;
 	}
 
 }
