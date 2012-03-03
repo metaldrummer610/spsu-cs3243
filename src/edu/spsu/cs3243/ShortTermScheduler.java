0b3966b4f8c04c05e3b7c9a40ad6c0970d4e07c5
metaldrummer610@gmail.com <metaldrummer610@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-03-03 21:53:14 +0000

    Adding stuff

--- src/edu/spsu/cs3243/ShortTermScheduler.java
+++ src/edu/spsu/cs3243/ShortTermScheduler.java
@@ -1,10 +1,26 @@
 package edu.spsu.cs3243;
 
 public class ShortTermScheduler {
+	private ScheduleType type;
 
-	public void load() {
-		// TODO Auto-generated method stub
-		
+	public enum ScheduleType {
+		FIFO
+	}
+
+	public ShortTermScheduler() {
+		type = ScheduleType.FIFO;
+	}
+
+	public void load(ProcessQueue readyQueue, ProcessQueue runningQueue) {
+		// Takes a process from the readyQueue and puts it on the runningQueue
+
+		switch (type) {
+		case FIFO:
+			PCB pcb = readyQueue.get(0);
+			// o.setStatus(ProcessStatus.Running);
+			runningQueue.add(pcb);
+			break;
+		}
 	}
 
 }
