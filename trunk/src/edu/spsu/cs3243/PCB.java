c260b9d7b2af615598292c59787808e13af2ab67
dbbutcher1@gmail.com <dbbutcher1@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-03-03 21:34:12 +0000

    Updated loader

--- trunk/src/edu/spsu/cs3243/PCB.java
+++ trunk/src/edu/spsu/cs3243/PCB.java
@@ -2,5 +2,16 @@ package edu.spsu.cs3243;
 
 public class PCB 
 {
+	int pid, size, priority, inputBuffer, outputBuffer, tempBuffer;
+	
+	public PCB()
+	{
+		pid = -1;
+		size = -1;
+		priority = -1;
+		inputBuffer = -1;
+		outputBuffer = -1;
+		tempBuffer = -1;
+	}
 
 }
