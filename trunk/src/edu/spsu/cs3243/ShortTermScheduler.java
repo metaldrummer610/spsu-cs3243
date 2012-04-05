bb0e48f6b52bbf0ae2e88b94181f2eb09c65da36
metaldrummer610@gmail.com <metaldrummer610@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-04-05 01:56:21 +0000

    Added the other two scheduling algorithms. Step 1 is done as far as I know

--- trunk/src/edu/spsu/cs3243/ShortTermScheduler.java
+++ trunk/src/edu/spsu/cs3243/ShortTermScheduler.java
@@ -1,10 +1,13 @@
 package edu.spsu.cs3243;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ShortTermScheduler {
-	private static ScheduleType type = ScheduleType.FIFO;
+	private static ScheduleType type = ScheduleType.PRIORITY;
 
 	public enum ScheduleType {
-		FIFO
+		FIFO, SJF, PRIORITY
 	}
 
 	public static synchronized PCB scheduleNextProcess() {
@@ -20,8 +23,61 @@ public class ShortTermScheduler {
 				readyQueue.processes.remove(p);
 				return p;
 			}
+			break;
+		case PRIORITY:
+			if (!readyQueue.processes.isEmpty()) {
+				Collections.sort(readyQueue.processes, new PriorityComparator());
+
+				PCB p = readyQueue.processes.get(0);
+				runningQueue.processes.add(p);
+				readyQueue.processes.remove(p);
+				System.out.println("Returning process: " + p.toString());
+				return p;
+			}
+			break;
+		case SJF:
+			if (!readyQueue.processes.isEmpty()) {
+				Collections.sort(readyQueue.processes, new SJFComparator());
+
+				PCB p = readyQueue.processes.get(0);
+				runningQueue.processes.add(p);
+				readyQueue.processes.remove(p);
+				System.out.println("Returning process: " + p.toString());
+				return p;
+			}
+			break;
 		}
 
 		return null;
+	}
+
+	public static class PriorityComparator implements Comparator<PCB> {
+		@Override
+		public int compare(PCB p1, PCB p2) {
+			int priority1 = p1.priority;
+			int priority2 = p2.priority;
+
+			if (priority1 < priority2)
+				return 1;
+			else if (priority1 > priority2)
+				return -1;
+			else
+				return 0;
+		}
+	}
+
+	public static class SJFComparator implements Comparator<PCB> {
+		@Override
+		public int compare(PCB p1, PCB p2) {
+			int processSize1 = p1.processSize;
+			int processSize2 = p2.processSize;
+
+			if (processSize1 > processSize2)
+				return 1;
+			else if (processSize1 < processSize2)
+				return -1;
+			else
+				return 0;
+		}
 	}
 }
