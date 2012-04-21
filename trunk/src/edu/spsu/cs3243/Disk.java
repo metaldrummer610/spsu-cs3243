9e7b67354d473e1d770ce7bba120b2c2973d6cda
k.debrito001@gmail.com <k.debrito001@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-04-21 02:15:22 +0000

    RAM and Disk done for now.....still working on long term scheduler

--- trunk/src/edu/spsu/cs3243/Disk.java
+++ trunk/src/edu/spsu/cs3243/Disk.java
@@ -3,8 +3,10 @@ package edu.spsu.cs3243;
 public class Disk {
 	private String[] disk;
 	private int size;
-	private int next_location;
+	//private int next_location;
 	private static Disk instance = null;
+	private int numPage;
+	private boolean[] full;
 
 	public static Disk instance() {
 		if (instance == null) {
@@ -17,30 +19,73 @@ public class Disk {
 	private Disk() {
 		disk = new String[2048];
 		size = 2048;
-		next_location = 0;
+		//next_location = 0;
+		numPage = (size/4);
+		for (int j = 0; j < numPage; j++) {
+			full[j] = false;
+		}
 	}
 
-	private Disk(int d) {
-		disk = new String[d];
-		size = disk.length;
-		next_location = 0;
-	}
+	public Disk (int s) {
+        disk = new String[s];   
+        size = disk.length;
+        //next_location = 0;
+        numPage=(s/4);
+        full = new boolean[numPage];
+        for (int j = 0; j<numPage; j++) 
+        {
+                full[j]=false;        
+        }
+}
+	
+	public int nextPage() 
+    {
+            for (int i=0;i<numPage;++i) 
+            {
+                    if(full[i]==false)
+                    {
+                            return i;
+                    }
+            }
+            return -1;
+    }
+
+
 
 	public int size() {
 		return size;
 	}
 
 	public boolean empty() {
-		return next_location == 0;
+		//return next_location == 0;
+		
+		 for (int j = 0;j < numPage; j++)
+         {
+                 if (full[j]==true) 
+                 {
+                         return false;
+                 }
+         }
+         return true; 
+		
+	
 	}
 
 	public boolean full() {
-		return next_location == size;
+		//return next_location == size;	
+		for (int j = 0;j < numPage;j++)
+        {
+                if (full[j]==false)
+                {
+                        return false;   
+                }                               
+        }               
+        return true; 
 	}
 
-	public int free() {
+	/*public int free() {
 		return 2048 - next_location;
-	}
+	}*/
 
 	public String toString() {
 		String temp_disk = "Disk Contents:";
@@ -48,7 +93,7 @@ public class Disk {
 			if (disk[i] != null) {
 				temp_disk += disk[i] + "\n";
 			}
-		temp_disk += next_location;
+		//temp_disk += next_location;
 		return temp_disk;
 
 	}
@@ -57,11 +102,16 @@ public class Disk {
 		for (int i = 0; i < disk.length; i++) {
 			disk[i] = "00000000";
 		}
-		next_location = 0;
+		//next_location = 0;     
+    	for(int j = 0;j < numPage; j++) {
+    	
+             full[j]=false;
+     }
+
 	}
 
 	// Write to Disk
-	public int write(String word) {
+	/*public int write(String word) {
 		if (full()) {
 			System.out.println("Disk Full");
 			return 0;
@@ -73,7 +123,32 @@ public class Disk {
 			return next_location - 1;
 		}
 
-	}
+	}*/
+	
+	public void write(String word, int here) {
+        if (here > -1 && here < size) {
+                disk[here] = word;
+                if (!full[here/4]) {
+                  full[here/4] = true;     
+                }
+        }
+        else 
+                System.out.println("Invalid Disk Writing Location");    
+}
+	
+	public void write(String word, int p, int o) 
+    {
+   
+            if ((p*4+o) < size() && (p*4+o) >= 0) {
+                    disk[(p*4+o)]=word;
+                    full[p] = true;         
+            }
+            else {
+                    System.out.println("Invalid Disk Writing Location");    
+            }
+    }
+
+
 
 	public String read(int address) {
 		if (address < 2048) {
