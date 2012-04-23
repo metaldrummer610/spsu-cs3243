b1d09d68cc41520cab1eb4f49bac80387ffb413c
k.debrito001@gmail.com <k.debrito001@gmail.com@27ff2df2-c90f-32d0-9209-fa076d36794c>
2012-04-23 02:58:18 +0000

    Long Term Scheduler done.......

--- trunk/src/edu/spsu/cs3243/LongTermScheduler.java
+++ trunk/src/edu/spsu/cs3243/LongTermScheduler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class LongTermScheduler {
 
+
 	public static void load() {
 		ProcessQueue newQueue = Driver.getNewQueue();
 		ProcessQueue readyQueue = Driver.getReadyQueue();
@@ -19,30 +20,41 @@ public class LongTermScheduler {
 				System.out.println("No RAM left");
 				break;
 			}
-
-			for (int current = job.instDiskLoc, i = job.processSize; i > 0; i--, current++) {
-				if (job.instMemLoc == -1) {
-					job.instMemLoc = RAM.instance().write(Disk.instance().read(current));
-				} else {
-					RAM.instance().write(Disk.instance().read(current));
+			
+			int currentPage = 0;
+			
+			for (int current = job.instDiskLoc; currentPage < 4; currentPage++) {
+				for (int currentOffset = 0; currentOffset < 4; currentOffset++) {
+					if ( currentPage == 0 && currentOffset == 0) {
+						job.instMemLoc = (RAM.instance().write(Disk.instance().read(current)));
+						job.pageTable.add(job.pageTable.size(), (job.instMemLoc));
+						job.pageFaultTable.add(job.pageFaultTable.size(), true);
+					}
+					
+					else if (currentOffset == 0) {
+						int wroteTo = RAM.instance().write(Disk.instance().read(current));
+						job.pageTable.add(job.pageTable.size(), (wroteTo));
+						job.pageFaultTable.add(job.pageFaultTable.size(), true);
+					}
+					
+					else { 
+						RAM.instance().write(Disk.instance().read(current), job.instMemLoc / 4 + currentPage, currentOffset);;
+					}
 				}
-
 			}
+			
+			for (;currentPage < Driver.getRunningQueue().largestJob()/4; currentPage++ ) {
+				job.pageTable.add(job.pageTable.size(), -1);
+				job.pageFaultTable.add(job.pageFaultTable.size(), false);
+			}	
 
-			for (int current = job.dataDiskLoc, i = job.dataSize; i > 0; i--, current++) {
-				if (job.dataMemLoc == -1) {
-					job.dataMemLoc = RAM.instance().write(Disk.instance().read(current));
-				} else {
-					RAM.instance().write(Disk.instance().read(current));
-				}
-			}
-
-			processes.add(job);
-		}
+		processes.add(job);
+		
 
 		newQueue.processes.removeAll(processes);
 		readyQueue.processes.addAll(processes);
 	}
+}
 
 	private static boolean loadProcess(ProcessQueue newQueue) {
 		if (newQueue.size() > 0) {
@@ -52,3 +64,24 @@ public class LongTermScheduler {
 		}
 	}
 }
+
+	//Phase 1 Long Term Scheduler
+
+			/*for (int current = job.instDiskLoc, i = job.processSize; i > 0; i--, current++) {
+				if (job.instMemLoc == -1) {
+					job.instMemLoc = RAM.instance().write(Disk.instance().read(current));
+				} else {
+					RAM.instance().write(Disk.instance().read(current));
+				}
+
+			}*/
+			
+			
+
+			/*for (int current = job.dataDiskLoc, i = job.dataSize; i > 0; i--, current++) {
+				if (job.dataMemLoc == -1) {
+					job.dataMemLoc = RAM.instance().write(Disk.instance().read(current));
+				} else {
+					RAM.instance().write(Disk.instance().read(current));
+				}
+			}*/
